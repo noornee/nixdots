2d1dc476c7daef0c64fe9a700d78c23086e4f558
noornee <noornee.011@gmail.com>
2024-10-30 14:51:31 +0100

    feat: add noto-fonts

--- home-manager/programs/font.nix
+++ home-manager/programs/font.nix
@@ -2,6 +2,7 @@
 
 {
   home.packages = with pkgs; [
+    noto-fonts
     noto-fonts-cjk-sans
     (nerdfonts.override { fonts = [ "Iosevka" "JetBrainsMono" ]; })
     corefonts # Times New Roman is amongst this
