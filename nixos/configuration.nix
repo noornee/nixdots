18f57ea05fb186a07da181a4a7857b3ab67c5da9
noornee <noornee.011@gmail.com>
2025-02-15 11:06:00 +0100

    chore: state version up

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -82,5 +82,5 @@
   };
 
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "24.05";
+  system.stateVersion = "24.11";
 }
