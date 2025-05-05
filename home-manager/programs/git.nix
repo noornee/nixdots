5f379f3458597c6f8e58fd34c509429ea2e044ec
noornee <noornee.011@gmail.com>
2025-05-05 15:37:41 +0100

    chore: add custom git alias(lg) for the pretty log output

--- home-manager/programs/git.nix
+++ home-manager/programs/git.nix
@@ -1,11 +1,15 @@
-{ userSettings, ... }:
-{
+{ userSettings, ... }: {
   programs.git = {
     enable = true;
     userName = userSettings.username;
     userEmail = userSettings.email;
     extraConfig = {
       init.defaultBranch = "main";
+      pull = { rebase = true; };
+    };
+    aliases = {
+      lg =
+        "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)'";
     };
   };
 }
