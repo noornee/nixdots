afa42c12550765bb993848b4c7a2e614e0c43c38
noornee <noornee.011@gmail.com>
2025-07-24 10:32:24 +0100

    add more keybinds to zellij

--- home-manager/programs/zellij/keybinds.nix
+++ home-manager/programs/zellij/keybinds.nix
@@ -101,6 +101,16 @@
               bind "Alt k" "Alt Up" { MoveFocus "Up"; }
               bind "Alt h" "Alt Left" { MoveFocusOrTab "Left"; }
               bind "Alt l" "Alt Right" { MoveFocusOrTab "Right"; }
+              bind "Alt Tab"  { GoToNextTab; }
+              bind "Alt 1"  { GoToTab 1; }
+              bind "Alt 2"  { GoToTab 2; }
+              bind "Alt 3"  { GoToTab 3; }
+              bind "Alt 4"  { GoToTab 4; }
+              bind "Alt 5"  { GoToTab 5; }
+              bind "Alt 6"  { GoToTab 6; }
+              bind "Alt 7"  { GoToTab 7; }
+              bind "Alt 8"  { GoToTab 8; }
+              bind "Alt 9"  { GoToTab 9; }
       	}
           shared_except "locked" {
               bind "Ctrl g" { SwitchToMode "Locked"; }
